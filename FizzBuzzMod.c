3e86b7cb2321578efd3b945d3ad243599649844e
Hiroaki Nitobe <h.nitobe@gmail.com>
2022-02-06 10:02:57 +0900

    Add files via upload

---
+++ FizzBuzzMod.c
@@ -0,0 +1,43 @@
+/**
+*	@file		FizzBuzzMod.c
+*	@brief		è—]‚ÅFizzBuzz
+*	@author		V“nŒËL–¾
+*	@date		2022/02/06
+*	@details	FizzBuzz‚ğè—]‚Å”»’è‚·‚é@Š„‡‘f’¼
+*/
+
+#include	<stdio.h>	// for printf()
+#include	<stdlib.h>	// for atoi()
+
+int main(int argc, char *argv[])
+{
+	int	fizz = 0, buzz = 0;
+	int	max, i;
+	
+	if (argc < 2) {		// ‘æ‚Qˆø”‚ª‘¶İ‚µ‚È‚¢
+		printf("g—p–@FFizzBuzzMod 16\n");
+		return -1;
+	}
+	
+	max = atoi(argv[1]);	// ‘æ‚Qˆø”‚ğ”’l‚ÅŠi”[@‘æ‚Rˆø”ˆÈ~‚Í–³‹‚µ‚Ü‚·
+	
+	for (i = 1; i <= max; i++) {
+		fizz = !(i % 3);	// ‚R‚Ì”{”H
+		buzz = !(i % 5);	// ‚T‚Ì”{”H
+	
+		if (fizz) {
+			printf("Fizz");
+		}
+		
+		if (buzz) {
+			printf("Buzz");
+		}
+		if (!fizz && !buzz) {
+			printf("%d", i);
+		}
+		printf(" ");
+	}
+	printf("\n");
+	
+	return 0;
+}
