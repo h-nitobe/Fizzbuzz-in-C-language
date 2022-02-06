997ab8953b1f20333adccb7d557ca6ebb3299867
Hiroaki Nitobe <h.nitobe@gmail.com>
2022-02-06 12:19:56 +0900

    Add files via upload

---
+++ FizzBuzzTbl.c
@@ -0,0 +1,45 @@
+/**
+*	@file		FizzBuzzTbl.c
+*	@brief		ƒe[ƒuƒ‹QÆ‚ÅFizzBuzz
+*	@author		V“nŒËL–¾
+*	@date		2022/02/06
+*	@details	
+*/
+
+#include	<stdio.h>	// for printf()
+#include	<stdlib.h>	// for atoi()
+
+//                          0  1  2  3  4  5  6  7  8  9 10 11 12 13 14
+static const char tbl[] = {15, 0, 0, 3, 0, 5, 3, 0, 0, 3, 5, 0, 3, 0, 0};
+
+int main(int argc, char *argv[])
+{
+	int	max, i;
+	
+	if (argc < 2) {		// ‘æ‚Qˆø”‚ª‘¶İ‚µ‚È‚¢
+		printf("g—p–@FFizzBuzzMod 16\n");
+		return -1;
+	}
+	
+	max = atoi(argv[1]);	// ‘æ‚Qˆø”‚ğ”’l‚ÅŠi”[@‘æ‚Rˆø”ˆÈ~‚Í–³‹‚µ‚Ü‚·
+	
+	for (i = 1; i <= max; i++) {
+		switch (tbl[i % 15]) {
+			case 3:
+				printf("Fizz " );
+				break;
+			case 5:
+				printf("Buzz ");
+				break;
+			case 15:
+				printf("FizzBuzz ");
+				break;
+			default:
+				printf("%d ", i);
+				break;
+		}
+	}
+	printf("\n");
+	
+	return 0;
+}
