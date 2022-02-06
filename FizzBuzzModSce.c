3bfa73931a3f298c34588b572ee6fd50cb0d9086
Hiroaki Nitobe <h.nitobe@gmail.com>
2022-02-06 10:03:11 +0900

    Add files via upload

---
+++ FizzBuzzModSce.c
@@ -0,0 +1,43 @@
+/**
+*	@file		FizzBuzzModSce.c
+*	@brief		剰余でFizzBuzz　短絡評価(short-circuit evaluation)
+*	@author		新渡戸広明
+*	@date		2022/02/06
+*	@details	FizzBuzzを剰余で判定する　短絡評価版　使っちゃダメ
+*			JIS X3010:2003の「6.5.13 論理AND演算子」
+*			ビット単位の2項 & 演算子と異なり，&& 演算子は左から右への評価を保証する。
+*			第一オペランドの評価の直後を副作用完了点とする。
+*			第1オペランドの値が0と比較して等しい場合，第2オペランドは評価しない。
+*/
+
+#include	<stdio.h>	// for printf()
+#include	<stdlib.h>	// for atoi()
+
+int main(int argc, char *argv[])
+{
+	int	fizz = 0, buzz = 0;
+	int	max, i;
+	
+	if (argc < 2) {		// 第２引数が存在しない
+		printf("使用法：FizzBuzzMod 16\n");
+		return -1;
+	}
+	
+	max = atoi(argv[1]);	// 第２引数を数値で格納　第３引数以降は無視します
+	
+	for (i = 1; i <= max; i++) {
+		fizz = !(i % 3);	// ３の倍数？
+		buzz = !(i % 5);	// ５の倍数？
+	
+		fizz && printf("Fizz");
+		
+		buzz && printf("Buzz");
+	
+		(!fizz && !buzz) && printf("%d", i);
+
+		printf(" ");
+	}
+	printf("\n");
+	
+	return 0;
+}
