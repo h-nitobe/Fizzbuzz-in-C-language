3376b87738b9f16de929cf082b5b151f40e4e170
Hiroaki Nitobe <h.nitobe@gmail.com>
2022-02-06 11:17:48 +0900

    Add files via upload

---
+++ FizzBuzzCnt.c
@@ -0,0 +1,45 @@
+/**
+*	@file		FizzBuzzCnt.c
+*	@brief		カウンタでFizzBuzz
+*	@author		新渡戸広明
+*	@date		2022/02/06
+*	@details	FizzBuzzをカウンタで判定する
+*/
+
+#include	<stdio.h>	// for printf()
+#include	<stdlib.h>	// for atoi()
+
+int main(int argc, char *argv[])
+{
+	int	fizz = 0, buzz = 0;
+	int	max, i;
+	
+	if (argc < 2) {		// 第２引数が存在しない
+		printf("使用法：FizzBuzzMod 16\n");
+		return -1;
+	}
+	
+	max = atoi(argv[1]);	// 第２引数を数値で格納　第３引数以降は無視します
+	
+	for (i = 1; i <= max; i++) {
+		fizz++;
+		buzz++;
+	
+		if (3 <= fizz) {
+			printf("Fizz");
+			fizz = 0;
+		}
+		
+		if (5 <= buzz) {
+			printf("Buzz");
+			buzz = 0;
+		}
+		if (fizz && buzz) {
+			printf("%d", i);
+		}
+		printf(" ");
+	}
+	printf("\n");
+	
+	return 0;
+}
