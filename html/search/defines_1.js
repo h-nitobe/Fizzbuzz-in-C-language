f1d966d9e4d9c02fbc2bec2fdae0db1505b17277
Hiroaki Nitobe <h.nitobe@gmail.com>
2022-02-10 11:33:25 +0900

    Delete html directory

--- html/search/defines_1.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['buzz_0',['BUZZ',['../_fizz_buzz_fin_8c.html#a4d7a42500434ec6947ccb8ac9ae931e6',1,'BUZZ():&#160;FizzBuzzFin.c'],['../_fizz_buzz_sft_8c.html#a4d7a42500434ec6947ccb8ac9ae931e6',1,'BUZZ():&#160;FizzBuzzSft.c']]]
-];
--- html/search/functions_1.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['div3_0',['div3',['../_fizz_buzz_mod_own_8c.html#a36cec33103f57d1f35ff8173596b5e03',1,'FizzBuzzModOwn.c']]],
-  ['div5_1',['div5',['../_fizz_buzz_mod_own_8c.html#ac372167603f1afb4434eba0ef67a6579',1,'FizzBuzzModOwn.c']]]
-];
