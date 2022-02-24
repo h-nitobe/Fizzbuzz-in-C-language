ea59771fba41c31f313a5232e03612721bf43238
Hiroaki Nitobe <h.nitobe@gmail.com>
2022-02-24 10:50:27 +0900

    new

---
+++ FizzBuzzVMAC.c
@@ -0,0 +1,14 @@
+#include	"stdio.h"
+
+main()
+{
+	int	i;
+	
+	for (i = 1; i <= 64; i++) {
+		if (!(i % 3)) printf("Fizz");
+		if (!(i % 5)) printf("Buzz");
+		if (i % 3 && i % 5) printf("%d", i);
+		printf(" ");
+	}
+	printf("\n");
+}
