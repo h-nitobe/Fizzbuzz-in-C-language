aba555180152ad36f3c2dd014b4003fadac6676c
Hiroaki Nitobe <h.nitobe@gmail.com>
2022-03-04 08:35:03 +0900

    new

---
+++ FizzBuzzGB.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+	int     i;
+
+	printf("* FizzBuzz on BGB * ");
+	printf("------------------- ");
+	printf("                    ");
+	
+	for (i = 1; i <= 64; i++) {
+		if (!(i % 3)) printf("Fizz");
+		if (!(i % 5)) printf("Buzz");
+		if (i % 3 && i % 5) printf("%d", i);
+		 printf(" ");
+	}
+	printf("\n");
+
+	return 0;
+}
