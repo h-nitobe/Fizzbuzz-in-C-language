0b5f03e22335e1381b1204dddd6ae1beb38cd573
h-nitobe <h.nitobe@gmail.com>
2022-04-21 05:06:47 +0900

    new

---
+++ FizzBuzzModTriPure2.c
@@ -0,0 +1,29 @@
+/**
+*	@file		FizzBuzzModTriPure.c
+*	@brief		剰余でFizzBuzz　三項演算子
+*	@author		2022仙台受講者
+*	@date		2022/04/20
+*	@details	三項演算子でがんばる
+*/
+
+#include	<stdio.h>	// for printf()
+#include	<stdlib.h>	// for atoi()
+
+int main(int argc, char *argv[])
+{
+	int	max, i;
+	
+	if (argc < 2) {		// 第２引数が存在しない
+		printf("使用法：" __FILE__ "\b\b 16\n");
+		return -1;
+	}
+	
+	max = atoi(argv[1]);	// 第２引数を数値で格納　第３引数以降は無視します
+	
+	for (i = 1; i <= max; i++) {
+		printf(i % 15 ? (i % 3 ? (i % 5 ? "%d ": "Buzz ") : "Fizz ") : "FizzBuzz ", i);
+	}
+	printf("\n");
+	
+	return 0;
+}
