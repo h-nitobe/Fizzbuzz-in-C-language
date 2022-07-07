b821f9517b42847e1e1e3e1e7645d5226e33a88d
Hiroaki Nitobe <h.nitobe@gmail.com>
2022-07-07 14:39:44 +0900

    new

---
+++ FizzBuzzRISCV.c
@@ -0,0 +1,16 @@
+#include        <stdio.h>
+
+int main(void)
+{
+        int     i;
+
+        for (i = 1; i <= 36; i++) {
+                if (!(i % 3)) printf("Fizz");
+                if (!(i % 5)) printf("Buzz");
+                if ((i % 3) && (i % 5)) printf("%d", i);
+                printf(" ");
+        }
+        printf("\n");
+
+        return 0;
+}
