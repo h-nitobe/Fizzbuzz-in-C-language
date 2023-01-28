1e4d1babd790710d2d8676922cdfe6152509be05
Hiroaki Nitobe <h.nitobe@gmail.com>
2023-01-28 14:58:44 +0900

    New

--- FizzBuzzPython.py
+++ FizzBuzzPython.py
@@ -9,10 +9,7 @@
 def fizzbuzz(max):
     for i in range(1, max+1) :
         msg = "fizz" * (i % 3 == 0) + "buzz" * (i % 5 == 0)
-        if msg:
-            print(msg, end=' ')
-        else:
-            print(i, end=' ')
+        print((msg if msg else i), end=' ')
     print('')
 
 fizzbuzz(100)
