39d36c67a02e70c6201e1e87f4721b79740fa38a
Hiroaki Nitobe <h.nitobe@gmail.com>
2023-01-28 09:27:03 +0900

    new

--- FizzBuzzJulia.jl
+++ FizzBuzzJulia.jl
@@ -1,3 +1,11 @@
+#=
+*	@file		FizzBuzzJulia.jl
+*	@brief		JuliaŒ¾Œê‚ÅFizzBuzz
+*	@author		V“nŒËL–¾
+*	@date		2023/01/07
+*	@details	FizzBuzz‚ğJuliaŒ¾Œê‚ÅÀ‘•‚·‚é
+=#
+
 function fizzbuzz(max)
         for i in 1:max
                 msg = "Fizz" ^ (i % 3 == 0) * "Buzz" ^ (i % 5 == 0)
---
+++ FizzBuzzPython.py
@@ -0,0 +1,18 @@
+#
+# @file         FizzBuzzPython.py
+# @brief        Python‚ÅFizzBuzz
+# @author       V“nŒËL–¾
+# @date         2023/01/28
+# @details      FizzBuzz‚ğPython‚ÅÀ‘•‚·‚é
+#
+
+def fizzbuzz(max):
+    for i in range(1, max+1) :
+        msg = "fizz" * (i % 3 == 0) + "buzz" * (i % 5 == 0)
+        if msg:
+            print(msg, end=' ')
+        else:
+            print(i, end=' ')
+    print('')
+
+fizzbuzz(100)
