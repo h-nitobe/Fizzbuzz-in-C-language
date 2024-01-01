04c758e6f0c38c309d264a49d0b0b4bb0387a966
Hiroaki Nitobe <h.nitobe@gmail.com>
2024-01-01 22:19:22 +0900

    Mod

--- FizzBuzzModOne.c
+++ FizzBuzzModOne.c
@@ -8,14 +8,28 @@
 *                       初学者がやりがちな圧縮記法を真似してみた
 *                       これだけ圧縮してもちゃんとコンパイルする
 *                       やるなあコンパイラ
+*
+*                       itoa() 非標準のCライブラリ関数
+*                       #include <stdlib.h>
+*                       char *itoa(int value, char *str, int radix);
+*                       戻り値：文字列へのポインタ（第二引数そのもの）
+*
 */
-#include	<stdio.h>
-#include	<stdlib.h>
+#include	<stdio.h>	// for printf()
+#include	<stdlib.h>	// for itoa()
 
-void main(void)
+void main(void)			// これ違反。int main(void) か int main(int argc, char *argv[])
 {
 	int		i;
 	char	b[32];
 	
 	for(i=0;100-i++;printf("%s%s%s ",i%3?"":"Fizz",i%5?"":"Buzz",i%3&&i%5?itoa(i,b,10):""));
+
+//	for (i = 0; 100 - i++;
+//		printf("%s%s%s ",
+//			i % 3? "": "Fizz",
+//			i % 5? "": "Buzz",
+//			i % 3 && i % 5? itoa(i,b,10): ""
+//		)
+//	);
 }
