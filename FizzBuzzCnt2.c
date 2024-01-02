cbe49cb873bfac4b90b696bd6f085f831a581f65
Hiroaki Nitobe <h.nitobe@gmail.com>
2024-01-02 17:15:25 +0900

    Mod

--- FizzBuzzCnt2.c
+++ FizzBuzzCnt2.c
@@ -1,26 +1,28 @@
 //
 // @file		FizzBuzzCnt2.c
-// @brief		カウンタでFizzBuzz 2
+// @brief		カウンタでFizzBuzz 2 ダウンカウンタ
 // @author		新渡戸広明
-// @date		2023/12/30
+// @date		2024/01/02
 // @details	
 //
 #include	<stdio.h>
 
+#define		CD(t, n)	t## = ##t##? ##t## - 1: n	// Fizz = Fizz? Fizz - 1: 2
+#define		FB(t)		printf(t##? "": #t)			// printf(Fizz? "": "Fizz")
+
 int main(void)
 {
-	int		fizz = 0, buzz = 0;
-	int		i = 0;
+	int		i = 0, Fizz = 0, Buzz = 0;
 	
 	while (100 - i++) {
-		if (3 <= ++fizz) fizz = 0;
-		if (5 <= ++buzz) buzz = 0;
-		printf(!fizz? "Fizz": "") |
-		printf(!buzz? "Buzz": "") ||
-		printf("%d", i);
+		CD(Fizz, 2);		// CountDown 210210210...
+		CD(Buzz, 4);		// CountDown 4321043210...
+		
+		FB(Fizz) | FB(Buzz) || printf("%d", i);
 		printf(" ");
 	}
 	printf("\n");
 	
 	return 0;
 }
+
