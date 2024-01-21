963b77e6465a877f2882a22b7aebe4dd0ee86c04
Hiroaki Nitobe <h.nitobe@gmail.com>
2024-01-22 08:09:45 +0900

    Mod

--- fizzbuzzRE2.sh
+++ fizzbuzzRE2.sh
@@ -3,9 +3,9 @@
 #       @file           fizzbuzzRE2.sh
 #       @brief          正規表現で FizzBuzz
 #       @author         唯物是真 @Scaled_Wurm
-#       		https://sucrose.hatenablog.com/entry/2018/02/28/231413
+#                       https://sucrose.hatenablog.com/entry/2018/02/28/231413
 #       @date           2018/02/28
 #       @details        2024/01/22 modefied by 新渡戸広明
-#       		chmod +x fizzbuzzRE2.sh を忘れずに
+#                       chmod +x fizzbuzzRE2.sh を忘れずに
 #
 seq 1 100 | perl -pe 's/^(\d*[05])$/$1Buzz/;s/^(?>([0369]|([147](?1)*)(?:([258](?1)*)|(?2){2})|(?3)(?:(?2)|(?3){2})))+(?=\D)/$1Fizz/;s/^\d+(?=B|F)//;s/\n/ /';echo ""
