92e37cd987920dd25ba4356f3dabc53ed9371d04
Hiroaki Nitobe <h.nitobe@gmail.com>
2025-02-15 17:19:34 +0900

    mod

--- FizzBuzz20250215.c
+++ FizzBuzz20250215.c
@@ -1,6 +1,17 @@
+/**
+*       @file           FizzBuzz20250215.c
+*       @brief          「憧れるのをやめましょう」（大谷翔平） FizzBuzz
+*       @author         新渡戸広明
+*       @date           2025/02/15
+*       @details        「憧れるのをやめましょう」（大谷翔平）
+*                       pythonには「スライス」が実装されていますが、
+*                       C言語の標準ライブラリには当該関数は存在しません。
+*                       なければ自分で作ればいいのよ！（涼宮ハルヒ）
+*                       というわけで憧れるのをやめてでっち上げてみた。
+*/
 #include    <stdio.h>
 
-int slice(char *str, int from, int to);
+int slice(char *str, int begin, int end);
 
 int main(void)
 {
@@ -19,18 +30,18 @@ int main(void)
     siice()
 
     引数：   *str : 文字列先頭アドレス
-            from : 印字開始位置
-            to   : 印字終了位置
+            begin : 印字開始位置
+            end   : 印字終了位置
     戻り値： 印字文字数
 
     2025/02/15  新渡戸広明
 */
-int slice(char *str, int from, int to)
+int slice(char *str, int begin, int end)
 {
     int i, len = 0;
 
-    for (i = from; i < to; i++) {
-        putchar(str[i]);
+    for (i = begin; i < end; i++) {
+        putchar(*(str + i));
         len++;
     }   
 
